f6e334d35145bae532bc1a521f540a5e6ef185a2
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-11-22 13:07:53 -0600

    Creacion de instancias de prueba para todo

--- src/index.js
+++ src/index.js
@@ -7,5 +7,9 @@ import {Todo,TodoList} from './class'
 const todoList = new TodoList();
 
 const tarea = new Todo('Aprender js');
+const tarea2 = new Todo('Aprender js');
 
-console.log( tarea);
+todoList.nuevoTodo( tarea );
+todoList.nuevoTodo( tarea2 );
+
+console.log( todoList );
