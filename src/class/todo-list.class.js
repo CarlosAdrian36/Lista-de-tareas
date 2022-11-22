25bb499b86b969483457e763b09cef6e00759b14
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-11-22 12:54:13 -0600

    metodos y funciones todo-list.class

---
+++ src/class/todo-list.class.js
@@ -0,0 +1,25 @@
+//Agrupar toda la lista de TODO
+
+export class TodoList {
+
+    constructor(){
+        this.todos = [];
+    }
+
+    nuevoTodo( todo ){
+        this.todos.push( todo );
+    }
+
+    eliminarTodo( id ){
+
+    }
+
+    marcarCompletado( id ){
+
+    }
+
+    eliminarCompletados(){
+        
+    }
+
+}
